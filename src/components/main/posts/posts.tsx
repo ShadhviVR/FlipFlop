ae999009fd0ecf45eeedc52bca20e53b9d7ea1ee
ShadhviVR <v.r.shadhvi@gmail.com>
2023-05-09 16:15:36 +0200

    post pics

--- src/components/main/posts/posts.tsx
+++ src/components/main/posts/posts.tsx
@@ -2,20 +2,20 @@ const posts = {
     1: {
       id: 1,
       name: "Drake",
-      picture: "src/assets/pro1.jpg",
+      picture: "https://source.unsplash.com/sibVwORYqs0",
       image:
-        "src/assets/posts/post1.jpg",
+        "https://source.unsplash.com/5Lw1U5BIumE",
       hashtag: ["#nature", "#winter"],
       comments: [
         {
           commentName: "Dalton",
-          commentPicture: "src/assets/pro11.jpg",
+          commentPicture: "https://source.unsplash.com/VVEwJJRRHgk",
           commentText: "Amazing!",
           commentID: 1,
         },
         {
           commentName: "Flynn",
-          commentPicture: "src/assets/pro10.jpg",
+          commentPicture: "https://source.unsplash.com/rDEOVtE7vOs",
           commentText: "What a beautiful place!",
           commentID: 2,
         },
@@ -24,20 +24,20 @@ const posts = {
     2: {
         id: 2,
         name: "Vienna",
-        picture: "src/assets/pro2.jpg",
+        picture: "https://source.unsplash.com/_H6wpor9mjs",
         image:
-          "src/assets/posts/post2.jpg",
+          "https://source.unsplash.com/kbc8fp9TcfA",
         hashtag: ["#winter", "#snow", "#sledge"],
         comments: [
           {
             commentName: "Pearl",
-            commentPicture: "src/assets/pro1.jpg",
+            commentPicture: "https://source.unsplash.com/VVEwJJRRHgk",
             commentText: "Time to sledge!",
             commentID: 1,
           },
           {
             commentName: "Clay",
-            commentPicture: "src/assets/pro10.jpg",
+            commentPicture: "https://source.unsplash.com/sibVwORYqs0",
             commentText: "Andy looks beautiful!",
             commentID: 2,
           },
@@ -46,20 +46,20 @@ const posts = {
       3: {
         id: 3,
         name: "Jenson",
-        picture: "src/assets/pro3.jpg",
+        picture: "https://source.unsplash.com/DItYlc26zVI",
         image:
-          "src/assets/posts/post3.png",
+          "https://source.unsplash.com/aN21jB8GfKs",
         hashtag: ["#christmas", "#christmastree"],
         comments: [
           {
             commentName: "Bryce",
-            commentPicture: "src/assets/pro9.jpg",
+            commentPicture: "https://source.unsplash.com/6anudmpILw4",
             commentText: "Nice decoration!",
             commentID: 1,
           },
           {
             commentName: "Jesicca",
-            commentPicture: "src/assets/pro10.jpg",
+            commentPicture: "https://source.unsplash.com/kvKSL7B6eTo",
             commentText: "Just wow!",
             commentID: 2,
           },
@@ -68,20 +68,20 @@ const posts = {
       4: {
         id: 4,
         name: "Hanna",
-        picture: "src/assets/pro4.jpg",
+        picture: "https://source.unsplash.com/rDEOVtE7vOs",
         image:
-          "src/assets/posts/post4.jpg",
+          "https://source.unsplash.com/SYx3UCHZJlo",
         hashtag: ["#summer", "#beach"],
         comments: [
           {
             commentName: "Alex",
-            commentPicture: "src/assets/pro8.jpg",
+            commentPicture: "https://source.unsplash.com/DItYlc26zVI",
             commentText: "Nice!",
             commentID: 1,
           },
           {
             commentName: "Monty",
-            commentPicture: "src/assets/pro7.jpg",
+            commentPicture: "https://source.unsplash.com/6anudmpILw4",
             commentText: "Holiday!",
             commentID: 2,
           },
@@ -90,20 +90,20 @@ const posts = {
       5: {
         id: 5,
         name: "Marcus",
-        picture: "src/assets/pro5.jpg",
+        picture: "https://source.unsplash.com/EQFtEzJGERg",
         image:
-          "src/assets/posts/post5.jpg",
+          "https://source.unsplash.com/NMLv5HQZnK4",
         hashtag: ["#spring", "#leaves"],
         comments: [
           {
             commentName: "Louise",
-            commentPicture: "src/assets/pro1.jpg",
+            commentPicture: "https://source.unsplash.com/_H6wpor9mjs",
             commentText: "Spring Time!",
             commentID: 1,
           },
           {
             commentName: "Nate",
-            commentPicture: "src/assets/pro2.jpg",
+            commentPicture: "https://source.unsplash.com/kvKSL7B6eTo",
             commentText: "Nature!",
             commentID: 2,
           },
@@ -112,20 +112,20 @@ const posts = {
       6: {
         id: 6,
         name: "Zoya",
-        picture: "src/assets/pro6.jpg",
+        picture: "https://source.unsplash.com/RQ2dvd2FUV4",
         image:
-          "src/assets/posts/post6.jpg",
+          "https://source.unsplash.com/oIuDXlOJSiE",
         hashtag: ["#northernlights"],
         comments: [
           {
             commentName: "Eric",
-            commentPicture: "src/assets/pro3.jpg",
+            commentPicture: "https://source.unsplash.com/DItYlc26zVI",
             commentText: "Wow!",
             commentID: 1,
           },
           {
             commentName: "Chirstina",
-            commentPicture: "src/assets/pro4.jpg",
+            commentPicture: "https://source.unsplash.com/0fN7Fxv1eWA",
             commentText: "The color though!",
             commentID: 2,
           },
@@ -134,20 +134,20 @@ const posts = {
       7: {
         id: 7,
         name: "Denver",
-        picture: "src/assets/pro7.jpg",
+        picture: "https://source.unsplash.com/6anudmpILw4",
         image:
-          "src/assets/posts/post7.jpg",
+          "https://source.unsplash.com/M4EcjtmE01k",
         hashtag: ["#amsterdam", "#boat"],
         comments: [
           {
             commentName: "Tabitha",
-            commentPicture: "src/assets/pro1.jpg",
+            commentPicture: "https://source.unsplash.com/RQ2dvd2FUV4",
             commentText: "Enjoy!",
             commentID: 1,
           },
           {
             commentName: "Winnie",
-            commentPicture: "src/assets/pro5.jpg",
+            commentPicture: "https://source.unsplash.com/EQFtEzJGERg",
             commentText: "Nice shot!",
             commentID: 2,
           },
@@ -156,20 +156,20 @@ const posts = {
       8: {
         id: 8,
         name: "Olivia",
-        picture: "src/assets/pro8.jpg",
+        picture: "https://source.unsplash.com/gxgGo5vk9Jw",
         image:
-          "src/assets/posts/post8.jpg",
-        hashtag: ["#coral", "#tortoise"],
+          "https://source.unsplash.com/0G01UI1MQhg",
+        hashtag: ["#coral", "#coralreef"],
         comments: [
           {
             commentName: "Macbeth",
-            commentPicture: "src/assets/pro4.jpg",
+            commentPicture: "https://source.unsplash.com/VVEwJJRRHgk",
             commentText: "Wow!",
             commentID: 1,
           },
           {
             commentName: "Andy",
-            commentPicture: "src/assets/pro6.jpg",
+            commentPicture: "https://source.unsplash.com/6anudmpILw4",
             commentText: "Nice shot!",
             commentID: 2,
           },
@@ -178,20 +178,20 @@ const posts = {
       9: {
         id: 9,
         name: "Yusuf",
-        picture: "src/assets/pro9.jpg",
+        picture: "https://source.unsplash.com/XHVpWcr5grQ",
         image:
-          "src/assets/posts/post9.jpg",
+          "https://source.unsplash.com/6PTiDHHnQrY",
         hashtag: ["#netherlands", "#tulip"],
         comments: [
           {
             commentName: "Joel",
-            commentPicture: "src/assets/pro3.jpg",
+            commentPicture: "https://source.unsplash.com/gxgGo5vk9Jw",
             commentText: "The color though!",
             commentID: 1,
           },
           {
             commentName: "Micky",
-            commentPicture: "src/assets/pro11.jpg",
+            commentPicture: "https://source.unsplash.com/EQFtEzJGERg",
             commentText: "Mind Blowing!",
             commentID: 2,
           },
@@ -200,20 +200,20 @@ const posts = {
       10: {
         id: 10,
         name: "Iva",
-        picture: "src/assets/pro10.jpg",
+        picture: "https://source.unsplash.com/kvKSL7B6eTo",
         image:
-          "src/assets/posts/post10.jpg",
-        hashtag: ["#paris", "#spring"],
+          "https://source.unsplash.com/-UWtb-3v6jQ",
+        hashtag: ["#paris", "#night"],
         comments: [
           {
             commentName: "John",
-            commentPicture: "src/assets/pro8.jpg",
+            commentPicture: "https://source.unsplash.com/XHVpWcr5grQ",
             commentText: "Lovely!",
             commentID: 1,
           },
           {
             commentName: "Jackie",
-            commentPicture: "src/assets/pro9.jpg",
+            commentPicture: "https://source.unsplash.com/6anudmpILw4",
             commentText: "Nice shot!",
             commentID: 2,
           },
@@ -222,20 +222,20 @@ const posts = {
       11: {
         id: 11,
         name: "Albert",
-        picture: "src/assets/pro11.jpg",
+        picture: "https://source.unsplash.com/EQFtEzJGERg",
         image:
-          "src/assets/posts/post11.jpg",
+          "https://source.unsplash.com/7QjU_u2vGDs",
         hashtag: ["#dog", "#cat"],
         comments: [
           {
             commentName: "Daisy",
-            commentPicture: "src/assets/pro7.jpg",
+            commentPicture: "https://source.unsplash.com/gxgGo5vk9Jw",
             commentText: "Friends!",
             commentID: 1,
           },
           {
             commentName: "Jane",
-            commentPicture: "src/assets/pro1.jpg",
+            commentPicture: "https://source.unsplash.com/0fN7Fxv1eWA",
             commentText: "Family!",
             commentID: 2,
           },
@@ -244,20 +244,20 @@ const posts = {
       12: {
         id: 12,
         name: "Amelie",
-        picture: "src/assets/pro12.jpg",
+        picture: "https://source.unsplash.com/RQ2dvd2FUV4",
         image:
-          "src/assets/posts/post12.jpg",
+          "https://source.unsplash.com/1MO9VGUC138",
         hashtag: ["#sunset", "#dog"],
         comments: [
           {
             commentName: "Cleo",
-            commentPicture: "src/assets/pro10.jpg",
+            commentPicture: "https://source.unsplash.com/EQFtEzJGERg",
             commentText: "Nice shot!",
             commentID: 1,
           },
           {
             commentName: "Dori",
-            commentPicture: "src/assets/pro6.jpg",
+            commentPicture: "https://source.unsplash.com/kvKSL7B6eTo",
             commentText: "Picture perfect!",
             commentID: 2,
           },
