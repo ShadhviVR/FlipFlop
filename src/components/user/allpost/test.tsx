06aaa5572d77eaa05d8516be6e00506f7f391553
ShadhviVR <v.r.shadhvi@gmail.com>
2023-05-09 15:34:20 +0200

    pics

--- src/components/user/allpost/test.tsx
+++ src/components/user/allpost/test.tsx
@@ -3,11 +3,11 @@ import { FiX } from "react-icons/fi";
 import tw from "tailwind-styled-components";
 
 const IMAGES = [
-  "src/assets/posts/post5.jpg",
+  "https://source.unsplash.com/C-YwiTWs_jg",
   "https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "src/assets/posts/post12.jpg",
+  "https://source.unsplash.com/MJCM35IItjk",
   "https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "src/assets/posts/post8.jpg",
+  "https://source.unsplash.com/vKcU0mb6cKg",
   "https://images.pexels.com/photos/1712/sunglasses-apple-iphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 ];
 
